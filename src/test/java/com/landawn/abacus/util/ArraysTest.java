bda4f7edd1fa7bdba029669cc00781d6cd8b7f3c
haiyangl <lihy70@hotamil.com>
2025-07-07 02:11:36 -0700

    aaa

--- src/test/java/com/landawn/abacus/util/ArraysTest.java
+++ src/test/java/com/landawn/abacus/util/ArraysTest.java
@@ -13,7 +13,7 @@
  */
 package com.landawn.abacus.util;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
